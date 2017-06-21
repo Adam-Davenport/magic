d0445d96e006d620e8e3f999b20b370320004e25
Adam-Davenport <adavenpo@purdue.edu>
2017-06-21 01:38:40 -0400

    Creating update script to get all magic cards and populate them into the database

---
+++ update.py
@@ -0,0 +1,21 @@
+from mtgsdk import Card as MagicCard
+import django
+import os
+
+
+def populate():
+    from cards.models import Card
+    print('Downloading all card data this will take some time...')
+    cards = MagicCard.all()
+    for c in cards:
+        card = Card.objects.create(
+            name=c.name,
+            set=c.set,
+            rarity=c.rarity
+        )
+        card.save()
+
+if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "magic.settings")
+    django.setup()
+    populate()

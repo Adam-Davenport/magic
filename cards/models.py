8f594b14dcf222746bc0ba1f73943a03cb62cd14
Adam-Davenport <adavenpo@purdue.edu>
2017-06-21 01:08:32 -0400

    Creating card model

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Card(models.Model):
+    name = models.CharField(max_length=100)
+    set = models.CharField(max_length=3)
+    rarity = models.CharField(max_length=10)

9582295e252022a28f74b33e93a918946295a95c
Adam Davenport <adavenpo@purdue.edu>
2017-06-21 16:37:25 -0400

    Creating view to show all cards in a set

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from cards.packs import get_set
+
 
 # Create your views here.
+def Set_View(request):
+    if request.method == 'GET':
+        card_set = get_set('AVR')
+        context = {
+            'card_set': card_set
+        }
+        return render(request, 'cards/set.html', context=context)
+    elif request.method == 'POST':
+        card_set = request.POST.set
+        return render(request, 'cards/set.html')

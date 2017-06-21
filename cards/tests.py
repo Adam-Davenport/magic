1b1c3f468118c1b78149019decc808c62189a882
Adam Davenport <adavenpo@purdue.edu>
2017-06-21 16:35:28 -0400

    Testing sets

--- cards/tests.py
+++ cards/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from cards.packs import get_set
+
 
 # Create your tests here.
+def test_set():
+    card_set = get_set('AVR')
+    for c in card_set:
+        print(c)
+
+if __name__ == '__main__':
+    test_set()

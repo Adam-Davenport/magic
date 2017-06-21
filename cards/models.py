2aa70b27b3e9c7acec9c3a00d3eb92a5e9aa3591
Adam Davenport <adavenpo@purdue.edu>
2017-06-21 16:35:03 -0400

    Adding str representation to Card model

--- cards/models.py
+++ cards/models.py
@@ -6,3 +6,6 @@ class Card(models.Model):
     name = models.CharField(max_length=100)
     set = models.CharField(max_length=3)
     rarity = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name

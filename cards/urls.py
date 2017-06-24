c506c68114e884a481a08363c1dd8e67db9899a4
Adam-Davenport <adavenpo@purdue.edu>
2017-06-24 10:13:15 -0400

    Adding booster battle url

--- cards/urls.py
+++ cards/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from cards import views
 
 urlpatterns = [
-    url(r'^set/$', views.Set_View, name='set')
+    url(r'^set/$', views.Set_View, name='set'),
+    # url(r'^$', views.Booster_View, name='booster'),
+    url(r'^boosterbattle', views.Booster_View, name='booster'),
 ]

8e4d964de4af1bfa4b96c59c6c42a58c67b121e7
Adam-Davenport <adavenpo@purdue.edu>
2017-06-24 00:36:32 -0400

    Creating view for booster battles

--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from cards.packs import get_set
+from cards.packs import get_set, Booster_Box
 
 
 # Create your views here.
@@ -13,3 +13,12 @@ def Set_View(request):
     elif request.method == 'POST':
         card_set = request.POST.set
         return render(request, 'cards/set.html')
+
+
+def Booster_View(request):
+    if request.method == 'GET':
+        boosters = Booster_Box('ATQ')
+        context = {
+            'boosters': boosters.packs
+        }
+        return render(request, 'cards/booster.html', context=context)

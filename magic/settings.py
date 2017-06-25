f486ad6c68c09e34b8aa27d9627d63069f5781ce
Adam-Davenport <adavenpo@purdue.edu>
2017-06-25 11:11:19 -0400

    Applying template dir to templates

--- magic/settings.py
+++ magic/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cards'
+    'cards',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'magic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

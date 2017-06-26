5c40e23949429db8400db93c9a32b083b1a9518d
Adam-Davenport <adavenpo@purdue.edu>
2017-06-26 12:16:48 -0400

    Removing setname field

--- cards/models.py
+++ cards/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Card(models.Model):
     name = models.CharField(max_length=100)
     set = models.CharField(max_length=3)
-    set_name = models.CharField(max_length=128)
     rarity = models.CharField(max_length=10)
 
     def __str__(self):

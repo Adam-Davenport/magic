49665818e2cf11468819bd553546912a96d58203
Adam-Davenport <adavenpo@purdue.edu>
2017-06-27 14:03:52 -0400

    Registering battle pack to admin site

--- cards/admin.py
+++ cards/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from cards.models import Card
+from cards.models import Card, Battle_Pack
 
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Battle_Pack)

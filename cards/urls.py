3a9a414f978400cfdb0da4fbdd259135802a6818
Adam-Davenport <adavenpo@purdue.edu>
2017-06-27 14:00:04 -0400

    Setting up media urls

--- cards/urls.py
+++ cards/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.conf.urls.static import staic
+from django.conf import settings
 from cards import views
 
 urlpatterns = [
     url(r'^set/$', views.Set_View, name='set'),
     # url(r'^$', views.Booster_View, name='booster'),
     url(r'^boosterbattle', views.Booster_View, name='booster'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

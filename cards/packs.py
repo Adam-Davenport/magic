fa849f64d1bac1ea81af4bfcf83cef38a470cc9b
Adam-Davenport <adavenpo@purdue.edu>
2017-06-28 16:15:40 -0400

    Adding booster box functionality

--- cards/packs.py
+++ cards/packs.py
@@ -129,11 +129,15 @@ def Create_Battle_Pack(pack, set_name):
 
 class Booster():
 
-    def __init__(self, set, amount):
+    def __init__(self, set_name, amount):
         self.packs = []
-        self.set = self.get_set()
+        self.set = self.get_set(set_name)
+        if amount == 36:
+            self.box()
+        else:
+            self.single(amount)
 
-    def get_set(self):
+    def get_set(self, set_name):
         cards = Card.objects.filter(set=set_name)
         mythics = []
         rares = []
@@ -150,23 +154,67 @@ class Booster():
                 commons.append(card)
         return Set(mythics, rares, uncommons, commons)
 
-    def single(self):
-        cards = []
-        # Commons
-        for c in range(1, 11):
-            index = randint(0, len(self.set.commons)-1)
-            cards.append(self.set.commons[index])
-        # Uncommons
-        for u in range(1, 3):
-            index = randint(0, len(self.set.uncommons)-1)
-            cards.append(self.set.uncommons[index])
-        # Rare
-        mythic_chance = randint(1, 7)
-        if mythic_chance == 7:
-            index = randint(0, len(self.set.mythics)-1)
-            mythic = self.set.mythics[index]
-            cards.append(mythic)
+    def single(self, amount):
+        for i in range(amount):
+            pack = []
+            # Commons
+            for c in range(1, 11):
+                index = randint(0, len(self.set.commons)-1)
+                pack.append(self.set.commons[index])
+            # Uncommons
+            for u in range(1, 3):
+                index = randint(0, len(self.set.uncommons)-1)
+                pack.append(self.set.uncommons[index])
+            # Rare
+            mythic_chance = randint(1, 7)
+            if mythic_chance == 7:
+                index = randint(0, len(self.set.mythics)-1)
+                mythic = self.set.mythics[index]
+                pack.append(mythic)
+            else:
+                index = randint(0, len(self.set.rares)-1)
+                pack.append(self.set.rares[index])
+            self.packs.append(pack)
+
+    def get_mythic_count(self):
+        if len(self.set.mythics) > 0:
+            rand = randint(0, 100)
+            if rand < 30:
+                return 4
+            elif rand < 60:
+                return 5
+            elif rand < 75:
+                return 3
+            elif rand < 90:
+                return 6
+            else:
+                return 7
         else:
-            index = randint(0, len(self.set.rares)-1)
-            cards.append(self.set.rares[index])
-        return cards
+            return 0
+
+    def box(self):
+        self.mythic_count = self.get_mythic_count()
+        for i in range(0, 36):
+            pack = []
+            # Commons
+            for c in range(1, 11):
+                index = randint(0, len(self.set.commons)-1)
+                pack.append(self.set.commons[index])
+            # Uncommons
+            for u in range(1, 3):
+                index = randint(0, len(self.set.uncommons)-1)
+                pack.append(self.set.uncommons[index])
+            # Rare
+            shuffle(self.set.mythics)
+            if self.mythic_count > 0:
+                index = randint(0, len(self.set.mythics)-1)
+                mythic = self.set.mythics[index]
+                self.mythic_count -= 1
+                pack.append(mythic)
+            else:
+                index = randint(0, len(self.set.rares)-1)
+                pack.append(self.set.rares[index])
+            self.packs.append(pack)
+        shuffle(self.packs)
+
+

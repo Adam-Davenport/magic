fd439e08805dceb9dbf436d23383408b1f321cc5
Adam-Davenport <adavenpo@purdue.edu>
2017-06-28 16:15:18 -0400

    Testing new booster class

--- cards/tests.py
+++ cards/tests.py
@@ -1,16 +1,11 @@
 from django.test import TestCase
-from cards.packs import Booster_Pack, Create_Battle_Pack, Booster_Box, get_set
+from cards.packs import Booster
 
 
 # Create your tests here.
 class BoosterTest(TestCase):
     def setUp(self):
-        current_set = 'AVR'
-        self.booster = Booster_Pack(current_set)
-        test_set = get_set('AVR')
-        print(len(test_set.commons))
-        # print(len(box.set.commons))
+        booster = Booster('LEA', 3)
 
     def test_battle_pack(self):
         print('test')
-        # Create_Battle_Pack(self.booster)

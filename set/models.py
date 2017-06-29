56c9b7f664f561b5709cfa977a32b7f5285ec3c5
Adam-Davenport <adavenpo@purdue.edu>
2017-06-29 14:41:47 -0400

    Fixing True spelling

--- set/models.py
+++ set/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Set(models.Model):
-    code = models.CharField(max_length=6, primary_key=true)
+    code = models.CharField(max_length=6, primary_key=True)
     name = models.CharField(max_length=128)
     gatherer_code = models.CharField(max_length=128)
     release_date = models.DateField()

bfb2ce7b46e69290bfd3630ce8fb9129885f8dc0
Adam-Davenport <adavenpo@purdue.edu>
2017-06-29 14:50:16 -0400

    Registering battle model

--- cards/admin.py
+++ cards/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from cards.models import Card, Battle_Pack
+from cards.models import Card, Battle, Battle_Pack
 
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Battle)
 admin.site.register(Battle_Pack)

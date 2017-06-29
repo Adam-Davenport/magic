709070e7b8f16477f0d05ccfba568f9413b3391c
Adam-Davenport <adavenpo@purdue.edu>
2017-06-28 21:38:03 -0400

    Changing set to set_name

--- cards/models.py
+++ cards/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Card(models.Model):
     name = models.CharField(max_length=100)
-    set = models.CharField(max_length=3)
+    set_name = models.CharField(max_length=3)
     rarity = models.CharField(max_length=10)
 
     def __str__(self):

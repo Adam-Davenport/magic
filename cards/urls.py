e1ceddc39017f80f2c479c3bcef8f3d327ea019a
Adam-Davenport <adavenpo@purdue.edu>
2017-06-30 17:01:58 -0400

    Changing booster battle view for lists

--- cards/urls.py
+++ cards/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^set/$', views.Set_View, name='set'),
     url(r'^$', views.Booster_View, name='index'),
     url(r'^boosters/$', views.Booster_View, name='boosters'),
-    url(r'^battles/$', views.Booster_View, name='battles'),
+    url(r'^battles/$', views.Battle_List_View, name='battles'),
     url(r'^battles/(?P<pk>\d)/$', views.Battle_View, name='battle_details'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

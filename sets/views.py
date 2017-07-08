beae2eee974b786daed4cfc957d9d64e0da44a93
Adam-Davenport <adavenpo@purdue.edu>
2017-07-07 23:38:43 -0400

    Creating index view

--- sets/views.py
+++ sets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from sets.manager import Get_Sets
 
-# Create your views here.
+
+def Index_View(request):
+    if request.method == 'GET':
+        sets = Get_Sets()
+        context = {
+            'sets': sets
+        }
+        return render(request, 'sets/index.html', content_type=context)

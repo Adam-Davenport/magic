3db89e54905ce674082695db4d8f02b794868118
Adam-Davenport <adavenpo@purdue.edu>
2017-07-07 21:19:58 -0400

    Creating file and method to get sets

---
+++ sets/manager.py
@@ -0,0 +1,9 @@
+import os
+
+
+def Get_Sets():
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sf_path = os.path.join(base_dir, 'static', 'sets.txt')
+    sf = open(sf_path, 'r')
+    sets = sf.readlines()
+    return sets

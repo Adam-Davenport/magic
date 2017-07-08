2729a2bb426671023fc329e8e921c1924b2cb992
Adam-Davenport <adavenpo@purdue.edu>
2017-07-07 20:44:54 -0400

    Renaming set app to sets

--- set/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Set(models.Model):
-    code = models.CharField(max_length=6, primary_key=True)
-    name = models.CharField(max_length=128)
-    gatherer_code = models.CharField(max_length=128)
-    release_date = models.DateField()

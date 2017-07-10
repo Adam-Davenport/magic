c06ca266fec5804899aad0e3f4423bb70fd17d0f
Adam-Davenport <adavenpo@purdue.edu>
2017-07-10 11:43:02 -0400

    Removing gatherer code and adding str representation

--- sets/models.py
+++ sets/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Set(models.Model):
     code = models.CharField(max_length=6, primary_key=True)
     name = models.CharField(max_length=128)
-    gatherer_code = models.CharField(max_length=128)
     release_date = models.DateField()
+
+    def __str__(self):
+        return self.name

124fa584636055acb7dfdbe0cde9efc0828f05b0
Adam-Davenport <adavenpo@purdue.edu>
2017-07-11 10:47:14 -0400

    Fixing file path

--- sets/manager.py
+++ sets/manager.py
@@ -3,7 +3,7 @@ import os
 
 def Get_Sets():
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sf_path = os.path.join(base_dir, 'static', 'sets.txt')
+    sf_path = os.path.join(base_dir, 'sets', 'static', 'sets.txt')
     sf = open(sf_path, 'r')
     sets = sf.readlines()
     return sets

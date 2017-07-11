c201981fe1fd9ec612dc595afdef5844b0ccebd8
Adam-Davenport <adavenpo@purdue.edu>
2017-07-11 11:01:57 -0400

    Getting sets to display in template

--- sets/views.py
+++ sets/views.py
@@ -7,10 +7,11 @@ def Index_View(request):
     if request.method == 'GET':
         try:
             setlist = Get_Sets()
-            sets = Set.objects.filter(code__in=setlist)
+            sets = Set.objects.all().order_by('release_date')
+            print(len(sets))
         except FileNotFoundError:
             sets = Set.objects.all()
         context = {
             'sets': sets
         }
-        return render(request, 'sets/index.html', content_type=context)
+        return render(request, 'sets/index.html', context=context)

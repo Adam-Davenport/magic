f4b70ff439c671cbf047994c21871c678880be89
Adam-Davenport <adavenpo@purdue.edu>
2017-07-11 10:40:41 -0400

    Filtering sets to list

--- sets/views.py
+++ sets/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from sets.manager import Get_Sets
+from sets.models import Set
 
 
 def Index_View(request):
     if request.method == 'GET':
-        sets = Get_Sets()
+        setlist = Get_Sets()
+        sets = Set.objects.filter(code__in=setlist)
         context = {
             'sets': sets
         }

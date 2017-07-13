b6a1dc8fb4dfc86d8a97e2339e3393e5552ce394
Adam-Davenport <adavenpo@purdue.edu>
2017-07-13 11:31:40 -0400

    Separating cards in set by rarity

--- sets/views.py
+++ sets/views.py
@@ -19,6 +19,25 @@ def Details_View(request, pk):
     if request.method == 'GET':
         current_set = get_object_or_404(Set, pk=pk)
         cards = Card.objects.filter(set_name=pk)
+        mythics = []
+        rares = []
+        uncommons = []
+        commons = []
+        for card in cards:
+            if card.rarity == 'Mythic Rare':
+                mythics.append(card)
+            elif card.rarity == 'Rare':
+                rares.append(card)
+            elif card.rarity == 'Uncommon':
+                uncommons.append(card)
+            elif card.rarity == 'Common':
+                commons.append(card)
+        cards = {
+            'mythics': mythics,
+            'rares': rares,
+            'uncommons': uncommons,
+            'commons': commons
+        }
         context = {
             'set': current_set,
             'cards': cards

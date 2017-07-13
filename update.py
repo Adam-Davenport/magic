272256123f30b0c5675336ba5f4767892821c46b
Adam-Davenport <adavenpo@purdue.edu>
2017-07-13 14:33:53 -0400

    Going back to full script

--- update.py
+++ update.py
@@ -7,11 +7,15 @@ def populate():
     from cards.models import Card
     print('Downloading all card data this will take some time...')
     cards = MagicCard.all()
+    # cards = MagicCard.where(supertypes='legendary') \
+    #     .where(types='creature') \
+    #     .where(colors='red,white') \
+    #     .all()
     print('Adding cards to database')
     for c in cards:
         card = Card.objects.create(
             name=c.name,
-            set=c.set,
+            set_name=c.set,
             rarity=c.rarity,
             image_url=c.image_url,
             multiverse_id=int(c.multiverse_id),

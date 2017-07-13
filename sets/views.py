a5d04f01d379c0c53f6c5a99ef101af29ee2b3ab
Adam-Davenport <adavenpo@purdue.edu>
2017-07-12 22:37:50 -0400

    Adding details view

--- sets/views.py
+++ sets/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from sets.manager import Get_Sets
 from sets.models import Set
+from cards.models import Card
 
 
 def Index_View(request):
     if request.method == 'GET':
-        try:
-            setlist = Get_Sets()
-            sets = Set.objects.all().order_by('release_date')
-            print(len(sets))
-        except FileNotFoundError:
-            sets = Set.objects.all()
+        sets = Set.objects.all()
+        for set in sets:
+            print(set.pk)
         context = {
             'sets': sets
         }
         return render(request, 'sets/index.html', context=context)
+
+
+def Details_View(request, pk):
+    if request.method == 'GET':
+        current_set = get_object_or_404(Set, pk=pk)
+        cards = Card.objects.filter(set_name=pk)
+        context = {
+            'set': current_set,
+            'cards': cards
+        }
+        return render(request, 'sets/details.html', context=context)
